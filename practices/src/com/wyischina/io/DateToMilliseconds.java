0e3b3c4ac82334d40747e306f570a7bc274da9a2
william.lee <william@breezecapsule.com>
2019-08-27 14:00:41 +0800

    updated README

---
+++ practices/src/com/wyischina/io/DateToMilliseconds.java
@@ -0,0 +1,12 @@
+package com.wyischina.io;
+
+import java.util.Scanner;
+
+public class DateToMilliseconds {
+
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        // read an integer
+        // print out the converted milliseconds
+    }
+}
