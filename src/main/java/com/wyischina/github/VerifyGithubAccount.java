9336f6b725507d56c0a7540aca2bc5e744aa87d0
William Lee <william@williamatic.com>
2019-08-29 17:16:31 +0800

    created a maven project instead.

---
+++ src/main/java/com/wyischina/github/VerifyGithubAccount.java
@@ -0,0 +1,9 @@
+package com.wyischina.github;
+
+public class VerifyGithubAccount {
+
+    public static void main(String[] args) {
+        // todo: replace this comment with some code that will define an integer variable 'i';
+    }
+
+}
