b6095f608acc59d62b6176b3a5f65fa620779a6a
John Lauron <jlauron21@students.wyischina.com>
2019-08-30 14:14:57 +0800

    test

--- src/main/java/com/wyischina/github/VerifyGithubAccount.java
+++ src/main/java/com/wyischina/github/VerifyGithubAccount.java
@@ -4,6 +4,7 @@ public class VerifyGithubAccount {
 
     public static void main(String[] args) {
         // todo: replace this comment with some code that will define an integer variable 'i';
+        // john's code
     }
 
 }
