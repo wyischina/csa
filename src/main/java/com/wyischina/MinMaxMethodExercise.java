e6f87ac7b4e3f61d3039d07026b0049004bec8a1
William Lee <william@williamatic.com>
2019-09-15 09:18:52 +0800

    added junit 5 tests.

---
+++ src/main/java/com/wyischina/MinMaxMethodExercise.java
@@ -0,0 +1,29 @@
+package com.wyischina;
+
+import java.util.Scanner;
+
+public class MinMaxMethodExercise {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter two numbers");
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int biggestNumber;
+        if (firstNumber > secondNumber) {
+            biggestNumber = firstNumber;
+        } else {
+            biggestNumber = secondNumber;
+        }
+        System.out.println("The biggest number is " + biggestNumber);
+    }
+
+    public static int findMaxNumber(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+}
