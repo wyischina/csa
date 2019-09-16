9b531267357d44062a85518b5de71b0a9262d786
sekim22 <sekim22@students.wyischina.com>
2019-09-16 15:02:49 +0800

    Shawn Kim SEK committing yay

--- src/main/java/com/wyischina/GithubExercise.java
+++ src/main/java/com/wyischina/GithubExercise.java
@@ -3,7 +3,7 @@ package com.wyischina;
 public class GithubExercise {
 
     public static void main(String[] args) {
-        // todo: print out your name
+        System.out.println("Shawn Kim");
     }
 
 }
