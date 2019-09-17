f813593018be8c7eb2af40a8e9304bf07bd31c04
sjyi22 <sjyi22@students.wyischina.com>
2019-09-17 18:10:00 +0800

    Jim Yi- finished (prime number)

--- src/main/java/com/wyischina/MinMaxMethodExercise.java
+++ src/main/java/com/wyischina/MinMaxMethodExercise.java
@@ -7,20 +7,19 @@ public class MinMaxMethodExercise {
     public static void main(String[] args) {
         System.out.println("Please enter two numbers");
         Scanner scanner = new Scanner(System.in);
-        int firstNumber = scanner.nextInt();
-        int secondNumber = scanner.nextInt();
-        int biggestNumber;
-        if (firstNumber > secondNumber) {
-            biggestNumber = firstNumber;
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        findMaxNumber(a, b);
+    }
+
+    static int findMaxNumber(int a, int b) {
+        if (a > b) {
+            System.out.println("greater number is: " + a);
+            return a;
         } else {
-            biggestNumber = secondNumber;
+            System.out.println("greater number is: " + b);
+            return b;
         }
-        System.out.println("max number is " + biggestNumber);
-    }
 
-    public static int findMaxNumber(int a, int b) {
-        // todo: complete this method to return the larger number of a and b.
-        return 0;
     }
-
 }
--- src/main/java/com/wyischina/PrimeMethodExercise.java
+++ src/main/java/com/wyischina/PrimeMethodExercise.java
@@ -5,13 +5,31 @@ import java.util.Scanner;
 public class PrimeMethodExercise {
 
     public static void main(String[] args) {
-        // todo: print out the first 10 primes from 0
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number.");
+        int a = scanner.nextInt();
+        isPrime(a);
     }
 
-    public boolean isPrime(int a) {
-        // todo: return true if a is prime, false otherwise
-        return false;
+    private static void isPrime(int a) {
+        int i = 2;
+        while (i <= a / 2) {
+            int q = a % i;
+            if (q == 0) {
+                if (i <= 10) {
+                    System.out.print(i+ ", ");
+                } else {
+                    System.out.println("true");
+                    break;
+                }
+            } else if (i == a / 2) {
+                System.out.println("false");
+                break;
+            }
+            i++;
+        }
     }
-
 }
+
+
+
