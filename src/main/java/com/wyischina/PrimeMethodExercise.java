650296bebdc870b7429ef72fed72f068fe1608e5
william.lee <william@breezecapsule.com>
2019-09-18 15:03:27 +0800

    added a prime method exercise.

--- src/main/java/com/wyischina/PrimeMethodExercise.java
+++ src/main/java/com/wyischina/PrimeMethodExercise.java
@@ -5,13 +5,24 @@ import java.util.Scanner;
 public class PrimeMethodExercise {
 
     public static void main(String[] args) {
-        // todo: print out the first 10 primes from 0
-
+        int found = 0;
+        int currentNumber = 1;
+        while(found < 10) {
+            if (isPrime(currentNumber)) {
+                System.out.println(currentNumber);
+                found++;
+            }
+            currentNumber++;
+        }
     }
 
     public static boolean isPrime(int a) {
-        // todo: return true if a is prime, false otherwise
-        return false;
+        for (int i = 2; i <= Math.sqrt(a); i = i+1) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
