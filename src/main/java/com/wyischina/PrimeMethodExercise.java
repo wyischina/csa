101a6b31b7f4658dfd46ad903a567e165aced26d
BuildTools <unconfigured@null.spigotmc.org>
2019-09-19 14:26:38 +0800

    Completed exercise

--- src/main/java/com/wyischina/PrimeMethodExercise.java
+++ src/main/java/com/wyischina/PrimeMethodExercise.java
@@ -1,17 +1,26 @@
 package com.wyischina;
 
-import java.util.Scanner;
-
 public class PrimeMethodExercise {
 
     public static void main(String[] args) {
         // todo: print out the first 10 primes from 0
-
+        int primeNums = 0;
+        for (int i = 2; primeNums < 10; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+                primeNums++;
+            }
+        }
     }
 
-    public boolean isPrime(int a) {
+    private static boolean isPrime(int a){
         // todo: return true if a is prime, false otherwise
-        return false;
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
