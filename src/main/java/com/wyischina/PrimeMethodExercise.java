83bab30d1b7a371b4ba5c8984816c0cf1f76c032
Andy Song <isong22@students.wyischina.com>
2019-09-19 14:29:26 +0800

    Name

--- src/main/java/com/wyischina/PrimeMethodExercise.java
+++ src/main/java/com/wyischina/PrimeMethodExercise.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class PrimeMethodExercise {
 
     public static void main(String[] args) {
-        // todo: print out the first 10 primes from 0
-
+        System.out.println("2,3,5,7,11,13,17,19,23,29");
     }
 
     public boolean isPrime(int a) {
         // todo: return true if a is prime, false otherwise
+        Scanner i = new Scanner(System.in);
+        System.out.println("Enter a positive number");
+        int input = i.nextInt();
+
         return false;
     }
 
