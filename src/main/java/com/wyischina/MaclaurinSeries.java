cc736846d6082728b993532299e647c72bfa714c
william.lee <william@breezecapsule.com>
2019-09-20 14:38:59 +0800

    maclaurin series

---
+++ src/main/java/com/wyischina/MaclaurinSeries.java
@@ -0,0 +1,10 @@
+package com.wyischina;
+
+public class MaclaurinSeries {
+
+    public double sine(double x, int n) {
+        // todo: calculate x to the nth term.
+        return 0.0;
+    }
+
+}
