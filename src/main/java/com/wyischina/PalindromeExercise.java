36544036987e425adcf785a62648b9bb54ba21b8
BuildTools <unconfigured@null.spigotmc.org>
2019-09-20 14:31:46 +0800

    use stringbuilder class and .append to create the reversed word. Added .trim() to check for whitespace strings such as "  ".

--- src/main/java/com/wyischina/PalindromeExercise.java
+++ src/main/java/com/wyischina/PalindromeExercise.java
@@ -12,15 +12,15 @@ public class PalindromeExercise {
      * @return true if word is a palindrome, false otherwise.
      */
     public static boolean isPalindrome(String word) {
-        String reversedWord = "";
+        StringBuilder reversedWord = new StringBuilder();
         word = word.toUpperCase();
-        if (word.equals("")){
+        if (word.trim().equals("")){
             return false;
         }
         for (int i = word.length(); i > 0; i--){
-            reversedWord = reversedWord.concat(word.substring(i-1, i));
+            reversedWord.append(word.substring(i - 1, i));
         }
-        return word.equals(reversedWord);
+        return word.equals(reversedWord.toString());
     }
 
 
