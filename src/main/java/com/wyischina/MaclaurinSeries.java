c6f896f7f4f8a78b1e3029dac63e2a8b091beadd
sjyi22 <sjyi22@students.wyischina.com>
2019-09-23 15:05:48 +0800

    maclaurin series.

--- src/main/java/com/wyischina/MaclaurinSeries.java
+++ src/main/java/com/wyischina/MaclaurinSeries.java
@@ -3,7 +3,13 @@ package com.wyischina;
 public class MaclaurinSeries {
 
     public double sine(double x, int k) {
-        return 0.0;
+        double acc = 0;
+        for (int i = 0; i <= k; i++) {
+            double numerator = Math.pow(-1, i) * Math.pow(x, 2 * i + 1);
+            double denominator = MethodCompositionExercise.factorial(2 * i + 1);
+            double sine = numerator / denominator;
+            acc = acc + sine;
+        }
+        return acc;
     }
-
 }
