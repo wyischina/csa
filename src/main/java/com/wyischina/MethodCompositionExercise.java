bafd093bf4a8bffa76a70ceb14b46966cd6e5b37
sekim22 <sekim22@students.wyischina.com>
2019-09-23 14:44:36 +0800

    commit

--- src/main/java/com/wyischina/MethodCompositionExercise.java
+++ src/main/java/com/wyischina/MethodCompositionExercise.java
@@ -5,18 +5,24 @@ public class MethodCompositionExercise {
 
     public static double sine(int x) {
         // todo: Express the maclaurin series for estimating sine(x) in terms of
+        int k = 0;
+        for(double i = 0; i <=3; i++){
+
+        }
         // a composition of the add/subtract/multiply/divide methods. Your expression should only
         // go up to the third term of the maclaurin series.
         //
         // i.e. x - x^3/3! + x^5/5!
 
-        return 0.0;
+        return factorial();
     }
 
-
     public static double factorial(double n) {
-        // todo: write a loop that computes the factorial of n
-        return 0;
+        double x = 1;
+        for(double i = 2; i<=n; i++) {
+            x = multiply(x,i);
+        }
+        return x;
     }
 
     public static double add(double a, double b) {
