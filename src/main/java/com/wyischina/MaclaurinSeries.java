0c2fb1b6702d7955e733d88dc80bf2ee674efbfc
Andy Song <isong22@students.wyischina.com>
2019-09-23 14:58:37 +0800

    Name

--- src/main/java/com/wyischina/MaclaurinSeries.java
+++ src/main/java/com/wyischina/MaclaurinSeries.java
@@ -4,10 +4,8 @@ public class MaclaurinSeries {
 
     public double sine(double x, int k) {
         double acc = 0;
-        double o = 2*k+1;
-
         for (int i = 0; i <= k; i++) {
-            acc = acc + (Math.pow(-1,i)*Math.pow(x, 2*k +1) / MethodCompositionExercise.factorial(2*k+1));
+            acc = acc + (Math.pow(-1,i)*Math.pow(x, (2*i)+1) / MethodCompositionExercise.factorial((2*i)+1));
         }
         return acc;
     }
