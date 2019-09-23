6d1c972e7202725655d06de6054620ffa7235fe8
John Lauron <jlauron21@students.wyischina.com>
2019-09-23 15:02:45 +0800

    This is my maclaurin series script.

--- src/main/java/com/wyischina/MaclaurinSeries.java
+++ src/main/java/com/wyischina/MaclaurinSeries.java
@@ -3,7 +3,21 @@ package com.wyischina;
 public class MaclaurinSeries {
 
     public double sine(double x, int k) {
-        return 0.0;
-    }
 
+        double total= 0;
+        for (int i = 0; i <= k; i++) {
+            double subterm = 2 * i + 1;
+            double term = Math.pow((-1), i) * (Math.pow(x, subterm)) / (factorial(subterm));
+            total = total + term;
+
+        }
+        return total;
+    }
+    public double factorial(double number) {
+        int fact = 1;
+        for (int i= 1; i <= number; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
 }
