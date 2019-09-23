6bb6670731062fdd1a37590f49faad746005673f
jsokol21 <jsokol21@students.wyischina.com>
2019-09-23 15:05:07 +0800

    commit

--- src/main/java/com/wyischina/MaclaurinSeries.java
+++ src/main/java/com/wyischina/MaclaurinSeries.java
@@ -3,7 +3,23 @@ package com.wyischina;
 public class MaclaurinSeries {
 
     public double sine(double x, int k) {
-        return 0.0;
+        double total = 0;
+
+        for(int i = 0; i <= k; i++){
+            double term = (Math.pow(-1, i) * Math.pow(x, 2*i+1)) / factorial(2*i+1);
+            total += term;
+        }
+        return total;
+    }
+
+    public static double factorial(int n) {
+        // todo: write a loop that computes the factorial of n
+        double factorial = 1.0;
+
+        for(int i = 2; i <= n; i++){
+            factorial *= i;
+        }
+        return factorial;
     }
 
 }
