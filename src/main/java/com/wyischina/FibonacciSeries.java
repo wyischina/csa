710379d83f22e4258730eec2d74fe4f53bc91f33
John Lauron <jlauron21@students.wyischina.com>
2019-09-24 14:21:35 +0800

    This is my maclaurin series script.

--- src/main/java/com/wyischina/FibonacciSeries.java
+++ src/main/java/com/wyischina/FibonacciSeries.java
@@ -4,7 +4,7 @@ public class FibonacciSeries {
 
     /**
      * Compute the nth fibonnaci number.
-     *
+     * <p>
      * fib(0) == 0
      * fib(1) == 1
      * fib(2) == 0 + 1 == 1
@@ -12,9 +12,23 @@ public class FibonacciSeries {
      * fib(n) == fib(n-1) + fib(n-2)
      *
      * @param n nth term
-     * @return the nth fibonnaci number
+     * @return
      */
+
+
     public static int fib(int n) {
-        return 0;
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        if (n < 2) {
+            return n;
+        } else {
+            for (int i= 0; i <= n-1; i++) {
+                c = a + b;
+                a = b;
+                b= c;
+            }
+        }
+        return c;
     }
 }
