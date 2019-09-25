c39de61241d1475034a7c80109d782edd3c81a60
william.lee <william@breezecapsule.com>
2019-09-25 15:02:01 +0800

    turtle ngon and circles.

---
+++ src/main/java/com/wyischina/TurtleConcentricCircles.java
@@ -0,0 +1,13 @@
+package com.wyischina;
+
+
+import java.awt.*;
+
+public class TurtleConcentricCircles {
+
+    public static void main(String[] args) {
+        Turtle turtle = new Turtle();
+        // todo: draw concentric circles
+    }
+
+}
