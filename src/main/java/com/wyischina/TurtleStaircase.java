aab3cd6019c325b2544dcb410fff72fe0fdcd319
jwang21 <jwang21@students.wyischina.com>
2019-09-26 14:19:56 +0800

    123

--- src/main/java/com/wyischina/TurtleStaircase.java
+++ src/main/java/com/wyischina/TurtleStaircase.java
@@ -7,8 +7,12 @@ public class TurtleStaircase {
     public static void main(String[] args) {
         Turtle turtle = new Turtle();
         Scanner scanner = new Scanner(System.in);
-        // todo: 1) read the number of steps from the user.
-        // todo: 2) draw out 5 staircase steps.
+        int n = scanner.nextInt();
+        for (int i = 0; i <= n; i++)
+        {
+            turtle.forward(50);
+            turtle.left(90);
+        }
     }
-
 }
+
