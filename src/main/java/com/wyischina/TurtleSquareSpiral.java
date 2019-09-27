45b55632a4822449e58607e6a4fa698b2b64559b
jwang21 <jwang21@students.wyischina.com>
2019-09-27 14:23:54 +0800

    123

--- src/main/java/com/wyischina/TurtleSquareSpiral.java
+++ src/main/java/com/wyischina/TurtleSquareSpiral.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class TurtleSquareSpiral {
 
     public static void main(String[] args) {
-        Turtle turtle = new Turtle();
-        // todo: use a loop to draw the square spiral.
+        Turtle bob = new Turtle();
+        
     }
-
 }
+
+
