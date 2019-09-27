8f5413ae3cc2475ded08a97313e88dbd5d1e159c
william.lee <william@breezecapsule.com>
2019-09-27 13:26:04 +0800

    pen width.

--- src/main/java/com/wyischina/TurtleConcentricCircles.java
+++ src/main/java/com/wyischina/TurtleConcentricCircles.java
@@ -7,14 +7,19 @@ public class TurtleConcentricCircles {
 
     public static void main(String[] args) {
         Turtle turtle = new Turtle();
-        for(int i = 0; i < 10; i++) {
-            drawCircle(turtle, 10 + 10 * i, 10 + i * 2);
+        final int numberOfCircles = 10;
+        final int penWidth = 15;
+        turtle.width(penWidth);
+        for(int i = 0; i < numberOfCircles; i++) {
+            float saturation = 1 - (i * 1.0f / numberOfCircles);
+            Color color = Color.getHSBColor(1, saturation, 1);
+            drawCircle(turtle, 10 + 10 * i, 10 + i * 2, color);
         }
     }
 
 
-    private static void drawCircle(Turtle t, int radius, int n) {
-        System.out.println("drawing circle with radius " + radius + " with " + n + "sides");
+    private static void drawCircle(Turtle t, int radius, int n, Color color) {
+        System.out.println("drawing circle with radius " + radius + " with " + n + " sides");
         double sectorAngle = 360.0 / n;
         double length = (radius * Math.asin(Math.toRadians(sectorAngle/2))) * 2;
         double innerAngle = (((n-2) * 180d) / n);
@@ -24,6 +29,7 @@ public class TurtleConcentricCircles {
         t.forward(radius);
         t.left(innerAngle / 2);
         t.down();
+        t.penColor(color);
         for (int i = 0; i < n; i++) {
             t.left(turnAngle);
             t.forward(length);
