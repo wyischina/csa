b2c2e91dce955a1ba684a6f224610c42ebe34ddc
Andy Song <isong22@students.wyischina.com>
2019-10-07 14:19:19 +0800

    Name

--- src/main/java/com/wyischina/TurtleNgon.java
+++ src/main/java/com/wyischina/TurtleNgon.java
@@ -6,18 +6,37 @@ public class TurtleNgon {
 
     public static void main(String[] args) {
         Turtle turtle = new Turtle();
-        System.out.println("How many sides would you like to draw?");
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        drawNgon(turtle, n, 100);
-
+        int n = 12;
+        drawNgon(turtle, n, 10);
     }
 
     public static void drawNgon(Turtle turtle, int n, int length) {
-        int i = 180-(n-2)*180 / n;
-        for (int x = 100; x < n; x++) {
-            turtle.forward(length);
-            turtle.left(i);
+        double i = 180-(((n-2)*180) / n);
+        double radius = (length * 360) / (2 * i * Math.PI);
+
+        for (int count = 0; count <10; count++) {
+            turtle.up();
+            turtle.right(90);
+            turtle.forward(radius);
+            turtle.down();
+            turtle.left(90);
+
+            for (int x = 0; x < n; x++) {
+                turtle.forward(length);
+                turtle.left(i);
+            }
+
+            n = n + 2;
+            i = 180-(((n-2)*180) / n);
+            turtle.up();
+            turtle.left(90);
+            turtle.forward(radius);
+            turtle.down();
+            turtle.right(90);
+
+            length = length + 1;
+
+            radius = (length * 360) / (2 * i * Math.PI);
         }
     }
 
