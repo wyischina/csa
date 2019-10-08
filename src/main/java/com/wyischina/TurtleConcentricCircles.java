b6f25a2aff1ad89b0369dec1cc34f84b80074ef8
jsokol21 <jsokol21@students.wyischina.com>
2019-10-08 15:07:02 +0800

    Here's the Concentric Circles

--- src/main/java/com/wyischina/TurtleConcentricCircles.java
+++ src/main/java/com/wyischina/TurtleConcentricCircles.java
@@ -4,19 +4,20 @@ package com.wyischina;
 import java.awt.*;
 
 //Credit where credit is due:
-//Much of this code is thanks to David... I almost figured it out, but David help me filled the logic gaps...
+//Much of this code is thanks to David... I almost figured it out, but David help me fill the logic gaps...
 
 public class TurtleConcentricCircles {
 
     public static void main(String[] args) {
         Turtle turtle = new Turtle();
         // todo: draw concentric circles
-        int n = 40;
+        //n is the number of sides that the circle must turn
+        int n = 30;
         double angle = 180 - (90 - 360/(double)(2*n));
-        int circNum = 10;
+        int circleNum = 10;
 
         //Drawing multiple circles
-        for(int i = 0; i < circNum; i++){
+        for(int i = 0; i < circleNum; i++){
             double radius = 10 + (10*i);
             turtle.up();
             turtle.home();
@@ -34,7 +35,7 @@ public class TurtleConcentricCircles {
         //Calculating the length that the turtle must move every time it turns
         double length = 2 * Math.sin(Math.PI/(n)) * radius;
         //Calculating the length that the turtle must turn every time
-        double angle = 180 - (n-2) * 180 / n;
+        double angle = 180 - (n-2) * 180 / (double) n;
         //Drawing the circle by repeating the turning of the angle and the moving of the length
         for(int i = 0; i < n; i++){
             turtle.forward(length);
