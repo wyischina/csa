739d41546dc129d09d730876c065a0ad3f530ca8
sjyi22 <sjyi22@students.wyischina.com>
2019-10-10 15:07:08 +0800

    Exception handling exercise.

--- src/main/java/com/wyischina/ExceptionHandlingExercise.java
+++ src/main/java/com/wyischina/ExceptionHandlingExercise.java
@@ -6,7 +6,19 @@ public class ExceptionHandlingExercise {
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        // todo
+        System.out.println("enter numerator");
+        int x = scanner.nextInt();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter denominator");
+        int y = scanner.nextInt();
+        try {
+            System.out.println("quotient: " + x/y);
+            int c = x % y;
+            System.out.println("remainder: " + c);
+        } catch (ArithmeticException e) {
+            System.out.println("re-enter the number");
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/com/wyischina/TurtleConcentricCircles.java
+++ src/main/java/com/wyischina/TurtleConcentricCircles.java
@@ -10,29 +10,54 @@ public class TurtleConcentricCircles {
         Turtle turtle = new Turtle();
         System.out.println("enter the radius of starting circle");
         Scanner scanner = new Scanner(System.in);
+
+        //The scanner gets the value of the radius for starting circle
+
         int n = scanner.nextInt();
+
+        //This calculation finds the distance turtle will move for 1 degrees left
+
         double x = (n * Math.PI) / 2;
         double y = x / 90;
-        //float dimmer = 100;
+        float dimmer = 1;
+        float color = 0.5f;
         turtle.up();
         turtle.forward(n);
         turtle.left(90);
-        //turtle.penColor(Color.getHSBColor(280, dimmer, 50));
+        turtle.penColor(Color.getHSBColor(color, dimmer, 0.5f));
         turtle.down();
-        for (int q = 0; q < 7; q++) {
-            for (int i = 0; i <= 360; i++) {
+        turtle.speed(1);
+
+        //this is the main loop for drawing ten circles
+
+        for (int q = 0; q < 10; q++) {
+
+            //this loop draws a circle in center, each time with increasing radius value.
+
+            for (int i = 0; i < 360; i++) {
                 turtle.forward(y);
                 turtle.left(1);
             }
-            //dimmer = dimmer - 15;
+            dimmer = dimmer - 0.15f;
+            color = color + 0.05f;
             turtle.up();
             turtle.right(90);
-            turtle.forward(20);
+            turtle.forward(10);
             turtle.left(90);
-            //turtle.penColor(Color.getHSBColor(280, dimmer, 50));
+            turtle.penColor(Color.getHSBColor(color, dimmer, 0.5f));
             turtle.down();
-            n = n + 20;
+
+            //this code increases the value for radius
+
+            n = n + 10;
             y = ((n * Math.PI) / 2) / 90;
         }
+
+        //after the loop, this code returns the turtle back to it's original position
+
+        turtle.up();
+        turtle.left(90);
+        turtle.forward(n);
+        turtle.left(180);
     }
 }
