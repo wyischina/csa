3b88c8a40618359e1c54571c526a00aeb3ac56f8
BuildTools <unconfigured@null.spigotmc.org>
2019-10-10 15:03:25 +0800

    Completed ExceptionHandlingExercise.java

--- src/main/java/com/wyischina/ExceptionHandlingExercise.java
+++ src/main/java/com/wyischina/ExceptionHandlingExercise.java
@@ -4,9 +4,20 @@ import java.util.Scanner;
 
 public class ExceptionHandlingExercise {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // todo
+
+        System.out.println("Please enter the first integer: ");
+        int a = scanner.nextInt();
+        System.out.println("Please enter the second integer: ");
+        int b = scanner.nextInt();
+
+        try{
+            System.out.println(a/b);
+            System.out.println(a%b);
+        } catch (ArithmeticException e){
+            System.out.println("Cannot divide integer by zero");
+        }
     }
 
 }
