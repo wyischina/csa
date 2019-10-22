354d8ef56fb67b47f471f49263c54eb56302b44a
wlee <william@breezecapsule.com>
2019-10-22 14:47:51 +0800

    bank design with hierachy

---
+++ src/test/java/com/wyischina/bank/AccountTest.java
@@ -0,0 +1,27 @@
+package com.wyischina.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AccountTest {
+
+    @Test
+    void testAccountCreation() {
+        Account account = new Account();
+        assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    void testAddDebitTransaction() {
+        Account account = new Account();
+        Transaction t = new Transaction();
+        t.isDebit = true;
+        t.amount = 1000;
+
+        assertEquals(0, account.getBalance());
+        account.addTransaction(t);
+        assertEquals(1000, account.getBalance());
+    }
+
+}
