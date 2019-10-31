b4c845c8c83a0c27d1c52b06b99ecfde8feb54cf
jsokol21 <jsokol21@students.wyischina.com>
2019-10-31 14:20:27 +0800

    Im finished

--- src/main/java/com/wyischina/array/MathTestResults.java
+++ src/main/java/com/wyischina/array/MathTestResults.java
@@ -1,38 +1,76 @@
 package com.wyischina.array;
 
+
 public class MathTestResults implements TestResults {
+    private StudentScore[] allScores = new StudentScore[10];
+    private int currentIndex = 0;
+
     @Override
     public void addScore(String studentName, int score) {
-
+        allScores[currentIndex] = new StudentScore(studentName, score);
+        currentIndex++;
     }
 
     @Override
     public int getNumberOfScores() {
-        return 0;
+        return currentIndex;
     }
 
     @Override
     public int getMaximumScore() {
-        return 0;
+        int max = 0;
+        for(int i = 0; i < allScores.length-1; i++){
+            max = Math.max(allScores[i].score,allScores[i+1].score);
+        }
+        return max;
     }
 
     @Override
     public int getMinimumScore() {
-        return 0;
+        int min = 0;
+        for(int i = 0; i < allScores.length-1; i++){
+            min = Math.min(allScores[i].score,allScores[i+1].score);
+        }
+        return min;
     }
 
     @Override
     public int getMeanScore() {
-        return 0;
+        int meanScore = 0;
+        int totalScoreValue = 0;
+        int currentIndex = allScores.length-1;
+
+        for(int i = 0; i < currentIndex;i++){
+            totalScoreValue += allScores[i].score;
+        }
+        return totalScoreValue/currentIndex;
     }
 
     @Override
     public String getStudentWithMaximumScore() {
-        return null;
+        int highestScore = -1;
+        String highestScorer = "";
+
+        for(int i = 0; i < allScores.length-1; i++){
+            if(allScores[i].score > highestScore){
+                highestScore = allScores[i].score;
+                highestScorer = allScores[i].studentName;
+            }
+        }
+        return highestScorer;
     }
 
     @Override
     public String[] getFailedStudents(int passScore) {
-        return new String[0];
+        int n = 1;
+        String[] failedStudents = new String[n];
+
+        for(int i = 0; i < allScores.length-1;i++){
+            if(allScores[i].score <= passScore){
+                n++;
+                failedStudents[n] = allScores[i].studentName;
+            }
+        }
+        return failedStudents;
     }
 }
---
+++ src/main/java/com/wyischina/array/StudentScore.java
@@ -0,0 +1,12 @@
+package com.wyischina.array;
+
+public class StudentScore {
+    public String studentName;
+    public int score;
+
+    //Constructor for StudentScore class
+    public StudentScore(String studentName, int score){
+        this.studentName = studentName;
+        this.score = score;
+    }
+}
