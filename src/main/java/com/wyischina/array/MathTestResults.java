591a51e8a847c649a05624908976bf705bfa7723
BuildTools <unconfigured@null.spigotmc.org>
2019-10-31 14:51:50 +0800

    Completed MathTestResults.java and tested 100%

--- src/main/java/com/wyischina/array/MathTestResults.java
+++ src/main/java/com/wyischina/array/MathTestResults.java
@@ -2,15 +2,20 @@ package com.wyischina.array;
 
 public class MathTestResults implements TestResults {
     private StudentScoreRecord[] studentScoreRecordArray = new StudentScoreRecord[10];
+    //keeps track of how many times addScore() has been called
     private int currentIndex = 0;
 
     @Override
     public void addScore(String studentName, int score) {
+        //check whether the list is full
         if(this.studentScoreRecordArray[this.studentScoreRecordArray.length-1] != null){
+            //create temporary array
             StudentScoreRecord[] tempStudentScoreRecordArray = new StudentScoreRecord[this.studentScoreRecordArray.length+10];
+            //loops through every value in the main array and assign each one to its corresponding position in the temporary array
             for(int i = 0; i < this.studentScoreRecordArray.length; i++){
                 tempStudentScoreRecordArray[i] = this.studentScoreRecordArray[i];
             }
+            //assigns the temporary array to the man array
             this.studentScoreRecordArray = tempStudentScoreRecordArray;
         }
         this.studentScoreRecordArray[this.currentIndex] = new StudentScoreRecord(studentName, score);
@@ -24,18 +29,18 @@ public class MathTestResults implements TestResults {
 
     @Override
     public int getMaximumScore() {
-        int max = 0;
-        for(int i = 1; i < currentIndex+1; i++){
-            max = Math.max(studentScoreRecordArray[i-1].getScore(), studentScoreRecordArray[i].getScore());
+        int max = Math.max(studentScoreRecordArray[0].getScore(), studentScoreRecordArray[1].getScore());
+        for(int i = 1; i < currentIndex; i++){
+            max = Math.max(max, studentScoreRecordArray[i].getScore());
         }
         return max;
     }
 
     @Override
     public int getMinimumScore() {
-        int min = 0;
-        for(int i = 1; i < currentIndex+1; i++){
-            min = Math.min(studentScoreRecordArray[i-1].getScore(), studentScoreRecordArray[i].getScore());
+        int min = Math.min(studentScoreRecordArray[0].getScore(), studentScoreRecordArray[1].getScore());
+        for(int i = 1; i < currentIndex; i++){
+            min = Math.min(min, studentScoreRecordArray[i].getScore());
         }
         return min;
     }
@@ -43,7 +48,7 @@ public class MathTestResults implements TestResults {
     @Override
     public int getMeanScore() {
         int total = 0;
-        for(int i = 1; i < currentIndex+1; i++) {
+        for(int i = 0; i < currentIndex; i++) {
             total += studentScoreRecordArray[i].getScore();
         }
         return total/(currentIndex);
@@ -52,8 +57,9 @@ public class MathTestResults implements TestResults {
     @Override
     public String getStudentWithMaximumScore() {
         String bestStudent = null;
-        for(int i = 0; i < currentIndex+1; i++) {
-            if(studentScoreRecordArray[i].getScore() == getMaximumScore()){
+        int max = getMaximumScore();
+        for(int i = 0; i < currentIndex; i++) {
+            if(studentScoreRecordArray[i].getScore() == max){
                 bestStudent = studentScoreRecordArray[i].getStudentName();
             }
         }
@@ -62,18 +68,20 @@ public class MathTestResults implements TestResults {
 
     @Override
     public String[] getFailedStudents(int passScore) {
+        //calculates the amount of failed students
         int stupidAmount = 0;
-        for(int i = 0; i < currentIndex+1; i++){
+        for(int i = 0; i < currentIndex; i++){
             if(studentScoreRecordArray[i].getScore() < passScore) {
                 stupidAmount++;
             }
         }
+        //put the students who failed into a list
         String[] stupidStudents = new String[stupidAmount];
-
-        for(int i = 0; i < currentIndex+1; i++) {
+        int tempIndex = 0;
+        for(int i = 0; i < currentIndex; i++) {
             if(studentScoreRecordArray[i].getScore() < passScore){
-                stupidStudents = new String[stupidStudents.length];
-                stupidStudents[i] = studentScoreRecordArray[i].getStudentName();
+                stupidStudents[tempIndex] = studentScoreRecordArray[i].getStudentName();
+                tempIndex++;
             }
         }
         return stupidStudents;
--- src/test/java/com/wyischina/array/MathTestResultsTest.java
+++ src/test/java/com/wyischina/array/MathTestResultsTest.java
@@ -2,13 +2,90 @@ package com.wyischina.array;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathTestResultsTest {
 
     @Test
-    public void testCreateMathTestResults() {
+    public void testConstructors() {
         TestResults testResults = new MathTestResults();
         assertEquals(0, testResults.getNumberOfScores());
+    }
+
+    @Test
+    public void testAddScore() {
+        TestResults testResults = new MathTestResults();
+        testResults.addScore("Dawei", 0);
+        testResults.addScore("one", 10);
+        testResults.addScore("two", 20);
+        assertEquals(3, testResults.getNumberOfScores());
+    }
+
+    @Test
+    public void testAddScoreArrayExtension(){
+        TestResults testResults = new MathTestResults();
+        for(int i = 0; i < 20; i++){
+            testResults.addScore("" + i, i * 10);
+        }
+        assertEquals(20, testResults.getNumberOfScores());
+    }
+
+    @Test
+    public void testMaxScore(){
+        TestResults testResults = new MathTestResults();
+        for(int i = 0; i < 20; i++){
+            if(i == 13){
+                testResults.addScore("max", 1000);
+            }
+            testResults.addScore("" + i, i * 10);
+        }
+        assertEquals(1000, testResults.getMaximumScore());
+    }
+
+    @Test
+    public void testMaxScoreStudent(){
+        TestResults testResults = new MathTestResults();
+        for(int i = 0; i < 20; i++){
+            if(i == 13){
+                testResults.addScore("max", 1000);
+            }
+            testResults.addScore("" + i, i * 10);
+        }
+        assertEquals("max", testResults.getStudentWithMaximumScore());
+    }
+
+    @Test
+    public void testMinScore(){
+        TestResults testResults = new MathTestResults();
+        for(int i = 0; i < 20; i++){
+            if(i == 13){
+                testResults.addScore("min", -1);
+            }
+            testResults.addScore("" + i, i * 10);
+        }
+        assertEquals(-1, testResults.getMinimumScore());
+    }
+
+    @Test
+    public void testMeanScore(){
+        TestResults testResults = new MathTestResults();
+        testResults.addScore("Sokol", 100);
+        testResults.addScore("Lauron", 13);
+        testResults.addScore("David", -1);
+        testResults.addScore("Jun", 200);
+
+        assertEquals(78, testResults.getMeanScore());
+    }
+
+    @Test
+    public void testStupidStudents(){
+        TestResults testResults = new MathTestResults();
+        testResults.addScore("Sokol", 100);
+        testResults.addScore("Lauron", 59);
+        testResults.addScore("David", -1);
+        testResults.addScore("Jun", 60);
+
+        assertArrayEquals(new String[]{"Lauron","David"}, testResults.getFailedStudents(60));
     }
 }
