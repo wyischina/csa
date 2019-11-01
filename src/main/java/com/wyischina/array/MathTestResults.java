ba82e33073495033fffc6fbd1feb08a22f37cb71
BuildTools <unconfigured@null.spigotmc.org>
2019-11-01 12:45:26 +0800

    Optimized code

--- src/main/java/com/wyischina/array/MathTestResults.java
+++ src/main/java/com/wyischina/array/MathTestResults.java
@@ -4,6 +4,10 @@ public class MathTestResults implements TestResults {
     private StudentScoreRecord[] studentScoreRecordArray = new StudentScoreRecord[10];
     //keeps track of how many times addScore() has been called
     private int currentIndex = 0;
+    private int max = 0;
+    private String studentMax;
+    private int min = 0;
+    private double mean = 0;
 
     @Override
     public void addScore(String studentName, int score) {
@@ -12,13 +16,29 @@ public class MathTestResults implements TestResults {
             //create temporary array
             StudentScoreRecord[] tempStudentScoreRecordArray = new StudentScoreRecord[this.studentScoreRecordArray.length+10];
             //loops through every value in the main array and assign each one to its corresponding position in the temporary array
-            for(int i = 0; i < this.studentScoreRecordArray.length; i++){
-                tempStudentScoreRecordArray[i] = this.studentScoreRecordArray[i];
-            }
+            System.arraycopy(this.studentScoreRecordArray, 0, tempStudentScoreRecordArray, 0, this.studentScoreRecordArray.length);
             //assigns the temporary array to the man array
             this.studentScoreRecordArray = tempStudentScoreRecordArray;
         }
         this.studentScoreRecordArray[this.currentIndex] = new StudentScoreRecord(studentName, score);
+
+        //find max student
+        if (this.max < score){
+            this.studentMax = studentName;
+        }
+        //find max
+        this.max = Math.max(this.max, score);
+        //find min
+        this.min = Math.min(this.min, score);
+        //find mean
+        if(currentIndex == 0){
+            this.mean = score;
+        } else {
+            this.mean = (score + (mean*currentIndex)) / (double)(currentIndex+1);
+        }
+
+
+
         this.currentIndex++;
     }
 
@@ -29,41 +49,27 @@ public class MathTestResults implements TestResults {
 
     @Override
     public int getMaximumScore() {
-        int max = Math.max(studentScoreRecordArray[0].getScore(), studentScoreRecordArray[1].getScore());
-        for(int i = 1; i < currentIndex; i++){
-            max = Math.max(max, studentScoreRecordArray[i].getScore());
-        }
         return max;
     }
 
     @Override
     public int getMinimumScore() {
-        int min = Math.min(studentScoreRecordArray[0].getScore(), studentScoreRecordArray[1].getScore());
-        for(int i = 1; i < currentIndex; i++){
-            min = Math.min(min, studentScoreRecordArray[i].getScore());
-        }
         return min;
     }
 
     @Override
     public int getMeanScore() {
-        int total = 0;
-        for(int i = 0; i < currentIndex; i++) {
-            total += studentScoreRecordArray[i].getScore();
+        if(mean - (int)mean >= 0.5){
+            return (int)(mean + 0.5);
         }
-        return total/(currentIndex);
+        else{
+            return (int)mean;
+        }
     }
 
     @Override
     public String getStudentWithMaximumScore() {
-        String bestStudent = null;
-        int max = getMaximumScore();
-        for(int i = 0; i < currentIndex; i++) {
-            if(studentScoreRecordArray[i].getScore() == max){
-                bestStudent = studentScoreRecordArray[i].getStudentName();
-            }
-        }
-        return bestStudent;
+        return studentMax;
     }
 
     @Override
@@ -85,5 +91,9 @@ public class MathTestResults implements TestResults {
             }
         }
         return stupidStudents;
+    }
+
+    public void removeScore(String studentName){
+
     }
 }
