c43db8d9eb3ef40c71a14d343836b7ecfa3e720e
william.lee <william@breezecapsule.com>
2019-11-07 12:39:52 +0800

    test results.

--- src/main/java/com/wyischina/array/ArrayExercise1.java
+++ src/main/java/com/wyischina/array/ArrayExercise1.java
@@ -8,7 +8,19 @@ public class ArrayExercise1 {
         System.out.println("Please enter the number n:");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
+        int[] numbers = new int[n];
+        for(int i = 0; i < n; i++) {
+            System.out.println("Please enter number " + (i + 1));
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.print("The numbers you have entered are ");
+        for(int i = n - 1; i >= 0; i--) {
+            System.out.print(numbers[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(".");
     }
 
 }
