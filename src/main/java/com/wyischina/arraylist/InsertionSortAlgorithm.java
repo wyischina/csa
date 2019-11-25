78d1ad8e14b255240a311886ba41aaac2d6703cf
william.lee <william@breezecapsule.com>
2019-11-25 14:07:06 +0800

    test

---
+++ src/main/java/com/wyischina/arraylist/InsertionSortAlgorithm.java
@@ -0,0 +1,11 @@
+package com.wyischina.arraylist;
+
+import java.util.List;
+
+public class InsertionSortAlgorithm implements SortingAlgorithm {
+    @Override
+    public List<Integer> sort(List<Integer> numbers) {
+        // todo: implement this
+        return null;
+    }
+}
