ff23a796ae4c4f1b3975f698da34128f31075a3f
william.lee <william@breezecapsule.com>
2019-12-06 15:06:36 +0800

    switch notation.

---
+++ src/main/java/com/wyischina/SwitchDemonstration.java
@@ -0,0 +1,19 @@
+package com.wyischina;
+
+public class SwitchDemonstration {
+
+    public static void main(String[] args) {
+        int value = 10;
+        switch(value) {
+            case 10:
+                System.out.println("case 10");
+                break;
+            case 11:
+                System.out.println("case 11");
+                break;
+            default:
+                System.out.println("default case");
+        }
+    }
+
+}
