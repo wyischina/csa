c718952d63581af45d07cff70232fdb30e465491
William Lee <william@williamatic.com>
2019-12-31 13:22:44 +0800

    maze app.

---
+++ src/main/java/com/wyischina/maze/MazeApp.java
@@ -0,0 +1,32 @@
+package com.wyischina.maze;
+
+/**
+ * Main class of the maze app.
+ */
+public class MazeApp {
+
+    /**
+     * Create a MazeApp.
+     */
+    public MazeApp() {
+
+    }
+
+
+    /**
+     * Run the maze app.
+     */
+    public void run() {
+
+    }
+
+
+    /**
+     * Run the app.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
